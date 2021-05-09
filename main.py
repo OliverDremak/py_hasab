af6bb9fa3ffae00cd0303919b548300715040b45
OliverDremak <72332685+OliverDremak@users.noreply.github.com>
2021-05-09 10:52:43 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+a=int(input("Kérem a háromszög a oldalát:"))
+b=int(input("Kérem a háromszög b oldalát:"))
+c=int(input("Kérem a háromszög c oldalát:"))
+
+terfogat=a*b*c
+felszin=2*(a*b+a*c+b*c)
+
+print(print(f'Térfogata:{terfogat}'))
+print(print(f'Felszine:{felszin}'))
